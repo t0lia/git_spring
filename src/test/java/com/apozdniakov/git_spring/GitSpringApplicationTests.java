85678e70b5385542e847f64b820d016c97029c56
bigtheta <bigtheta.org@gmail.com>
2021-09-12 08:28:37 +0300

    git maven plugin test

---
+++ src/test/java/com/apozdniakov/git_spring/GitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apozdniakov.git_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
