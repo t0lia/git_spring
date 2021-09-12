a0b64e57d5fb79e6032e96f5472c7bd21870f63b
bigtheta <bigtheta.org@gmail.com>
2021-09-12 08:53:37 +0300

    setup git info

--- src/main/java/com/apozdniakov/git_spring/GitSpringApplication.java
+++ src/main/java/com/apozdniakov/git_spring/GitSpringApplication.java
@@ -2,12 +2,26 @@ package com.apozdniakov.git_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class GitSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringApplication.class, args);
+    }
+//
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+//        PropertySourcesPlaceholderConfigurer propsConfig
+//                = new PropertySourcesPlaceholderConfigurer();
+//        propsConfig.setLocation(new ClassPathResource("git.properties"));
+//
+//        propsConfig.setIgnoreResourceNotFound(true);
+//        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+//        return propsConfig;
+//    }
 
 }
